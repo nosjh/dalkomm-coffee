56770e71382c3c6d01b44b20686dfe5c3d3076bf
nosjh <sonjunee34@gmail.com>
2022-04-12 18:21:26 +0900

    0412

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+var mainSwiper = new Swiper(".mySwiper", {
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  autoplay: {
+      delay: 2000,
+  },
+  speed: 800,
+  spaceBetween: 24,
+  slidesPerView: "auto",
+});
