79ed51696418c6049126f7ed1bdc607871b0e864
nosjh <sonjunee34@gmail.com>
2022-04-13 18:01:36 +0900

    0413

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ var mainSwiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   autoplay: {
       delay: 2000,
   },
