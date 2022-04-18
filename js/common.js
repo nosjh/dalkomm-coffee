772dfb385b04a0aea3e8ece8d1f3ea43e425d866
nosjh <sonjunhee34@gmail.com>
2022-04-18 15:11:19 +0900

    04-18

--- js/common.js
+++ js/common.js
@@ -1,6 +1,25 @@
+// 모바일 메뉴 토글
 $(".toggle").on('click', function() {
     $(this).toggleClass("on");
     $('.nav_detail').toggleClass("on");
     $(".menu .menu__main").toggleClass('hidden');
     $(".main__banner").toggleClass('hidden');
+});
+
+// 모바일 하위메뉴 보이기 숨기기
+const showHide = e => {
+    const MListDrop = e.children[1];
+    const displayAttr = window.getComputedStyle(MListDrop).display;
+
+    if (displayAttr == 'none') {
+        MListDrop.style.display = 'block';
+    } else {
+        MListDrop.style.display = 'none';
+    }
+};
+
+// 모바일 메뉴 선택시 색상변경
+$('.main__list a').click(function(){
+    $('.main__list a').removeClass("selected");
+    $(this).addClass("selected");
 });
