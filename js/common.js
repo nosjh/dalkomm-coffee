1c87b3a61d9116a31c6015f1688682c0656a190a
nosjh <sonjunhee34@gmail.com>
2022-04-19 19:01:07 +0900

    04-19

--- js/common.js
+++ js/common.js
@@ -6,20 +6,88 @@ $(".toggle").on('click', function() {
     $(".main__banner").toggleClass('hidden');
 });
 
-// 모바일 하위메뉴 보이기 숨기기
-const showHide = e => {
-    const MListDrop = e.children[1];
-    const displayAttr = window.getComputedStyle(MListDrop).display;
+// 모바일 하위메뉴 토글
+// if(matchMedia("screen and (max-width: 1023px)").matches) {
+// $( ".list__drop").hide();
+// $( ".show-list" ).parent().on('click', function() {
+//     $(this).find(".show-list i").toggleClass("down");
+//     $(this).find(".list__drop").toggle();
+// }); }
 
-    if (displayAttr == 'none') {
-        MListDrop.style.display = 'block';
+// window.addEventListener("optimizedResize", function() {
+//     let winWidth = window.innerWidth;
+
+//     if (winWidth < 1024) {
+//         $( ".list__drop").hide();
+//         $( ".show-list" ).parent().on('click', function() {
+//             $(this).find(".show-list i").toggleClass("down");
+//             $(this).find(".list__drop").toggle();
+//         });
+//     }
+// });
+
+
+if (window.innerWidth < 1024) {
+    $(".list__drop").css("display", "");
+    $( ".list__drop").hide();
+    $( ".show-list" ).parent().on('click', function() {
+        $(this).find(".show-list i").toggleClass("down");
+
+
+       const view = $(this).find(".list__drop").css("display");
+
+       if(view == 'none') {
+            $(this).find(".list__drop").show(0, function() {
+                // $(this).css("display", "");
+            });
+       } else {
+            $(this).find(".list__drop").hide(0, function() {
+                // $(".list__drop").css("display", "");
+            });
+       }
+
+        // $(this).find(".list__drop").css("display", "block");
+
+        // $(this).find(".list__drop").toggle();
+    });
+} else {
+    $(".list__drop").css("display", "");
+}
+
+
+$(window).resize(function () {
+    if (window.innerWidth < 1024) {
+        $(".list__drop").css("display", "");
+        $( ".list__drop").hide();
+        $( ".show-list" ).parent().on('click', function() {
+            $(this).find(".show-list i").toggleClass("down");
+
+            const view = $(this).find(".list__drop").css("display");
+
+            if(view == 'none') {
+                 $(this).find(".list__drop").show(0, function() {
+                    //  $(this).css("display", "");
+                 });
+            } else {
+                 $(this).find(".list__drop").hide(0, function() {
+                    //  $(".list__drop").css("display", "");
+                 });
+            }
+            // $(this).find(".list__drop").toggle();
+
+            // $(this).find(".list__drop").css("display")
+
+            // $(this).find(".list__drop").css("display", "block");
+        });
     } else {
-        MListDrop.style.display = 'none';
+        $(".list__drop").css("display", "");
     }
-};
+});
 
 // 모바일 메뉴 선택시 색상변경
 $('.main__list a').click(function(){
     $('.main__list a').removeClass("select");
     $(this).addClass("select");
 });
+
+
