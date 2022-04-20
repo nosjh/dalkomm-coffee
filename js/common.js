7689999e4b82ad1dacb5f0695df260f5c052563a
nosjh <sonjunhee34@gmail.com>
2022-04-20 15:24:31 +0900

    04-20

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 // 모바일 메뉴 토글
 $(".toggle").on('click', function() {
+    $(".list__drop").css("display", "");
     $(this).toggleClass("on");
     $('.nav_detail').toggleClass("on");
     $(".menu .menu__main").toggleClass('hidden');
@@ -7,82 +8,27 @@ $(".toggle").on('click', function() {
 });
 
 // 모바일 하위메뉴 토글
-// if(matchMedia("screen and (max-width: 1023px)").matches) {
-// $( ".list__drop").hide();
-// $( ".show-list" ).parent().on('click', function() {
-//     $(this).find(".show-list i").toggleClass("down");
-//     $(this).find(".list__drop").toggle();
-// }); }
-
-// window.addEventListener("optimizedResize", function() {
-//     let winWidth = window.innerWidth;
-
-//     if (winWidth < 1024) {
-//         $( ".list__drop").hide();
-//         $( ".show-list" ).parent().on('click', function() {
-//             $(this).find(".show-list i").toggleClass("down");
-//             $(this).find(".list__drop").toggle();
-//         });
-//     }
-// });
-
-
-if (window.innerWidth < 1024) {
-    $(".list__drop").css("display", "");
-    $( ".list__drop").hide();
     $( ".show-list" ).parent().on('click', function() {
-        $(this).find(".show-list i").toggleClass("down");
-
-
-       const view = $(this).find(".list__drop").css("display");
-
-       if(view == 'none') {
-            $(this).find(".list__drop").show(0, function() {
-                // $(this).css("display", "");
-            });
-       } else {
-            $(this).find(".list__drop").hide(0, function() {
-                // $(".list__drop").css("display", "");
-            });
-       }
-
-        // $(this).find(".list__drop").css("display", "block");
-
-        // $(this).find(".list__drop").toggle();
-    });
-} else {
-    $(".list__drop").css("display", "");
-}
-
-
-$(window).resize(function () {
-    if (window.innerWidth < 1024) {
-        $(".list__drop").css("display", "");
-        $( ".list__drop").hide();
-        $( ".show-list" ).parent().on('click', function() {
+        if (window.innerWidth < 1024) { 
             $(this).find(".show-list i").toggleClass("down");
-
+            
+            
             const view = $(this).find(".list__drop").css("display");
-
+            
             if(view == 'none') {
-                 $(this).find(".list__drop").show(0, function() {
-                    //  $(this).css("display", "");
-                 });
+                $(this).find(".list__drop").show();
             } else {
-                 $(this).find(".list__drop").hide(0, function() {
-                    //  $(".list__drop").css("display", "");
-                 });
+                $(this).find(".list__drop").hide();
             }
-            // $(this).find(".list__drop").toggle();
+        }
+    });
 
-            // $(this).find(".list__drop").css("display")
+    $(window).resize(function () {
+        if (window.innerWidth >= 1024) {  
+            $(".list__drop").css("display", "");
+        }
+    });
 
-            // $(this).find(".list__drop").css("display", "block");
-        });
-    } else {
-        $(".list__drop").css("display", "");
-    }
-});
 
 // 모바일 메뉴 선택시 색상변경
 $('.main__list a').click(function(){
